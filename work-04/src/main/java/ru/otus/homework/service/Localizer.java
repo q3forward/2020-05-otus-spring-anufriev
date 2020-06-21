697b30e8536a84852a5bb594fb85c2e30643c7ba
Forward <q3forward@gmail.com>
2020-06-22 00:23:00 +0300

    Добавлен интерфейс Loсalizer, замокан MessageSource в тесте

--- work-04/src/main/java/ru/otus/homework/service/Localizer.java
+++ work-04/src/main/java/ru/otus/homework/service/Localizer.java
@@ -1,32 +1,12 @@
 package ru.otus.homework.service;
 
-import org.springframework.context.MessageSource;
-import org.springframework.stereotype.Service;
-import ru.otus.homework.config.ApplicationSettings;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Service
-public class Localizer {
+public interface Localizer {
 
-    private final MessageSource messageSource;
-    private final ApplicationSettings appSettings;
+    List<String> localize(List<String> strs);
 
-    public Localizer(ApplicationSettings appSettings, MessageSource messageSource) {
-        this.appSettings = appSettings;
-        this.messageSource = messageSource;
-    }
+    String localize(String str);
 
-    public List<String> localize(List<String> strs) {
-        return strs.stream().map(str -> messageSource.getMessage(str, null, appSettings.getLocale())).collect(Collectors.toList());
-    }
-
-    public String localize(String str) {
-        return str!=null ? messageSource.getMessage(str, null, appSettings.getLocale()) : null;
-    }
-
-    public String localize(String str, Object... values) {
-        return str!=null ? messageSource.getMessage(str, values, appSettings.getLocale()) : null;
-    }
+    String localize(String str, Object... values);
 }
--- work-04/src/test/java/ru/otus/homework/service/LocalizerImplTest.java
+++ work-04/src/test/java/ru/otus/homework/service/LocalizerImplTest.java
@@ -1,12 +1,14 @@
 package ru.otus.homework.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import ru.otus.homework.config.ApplicationSettings;
 
 import java.util.Arrays;
@@ -14,12 +16,17 @@ import java.util.List;
 import java.util.Locale;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 
-@DisplayName("Class Localizer")
+@DisplayName("Class LocalizerImpl")
 @SpringBootTest
-public class LocalizerTest {
+public class LocalizerImplTest {
+
+    @Configuration
+    @Import({LocalizerImpl.class, MessageSourceAutoConfiguration.class})
+    class TestContextConfig {
+    }
 
     private final Locale ruLoc = new Locale("ru");
     private final Locale enLoc = Locale.ENGLISH;
@@ -27,7 +34,7 @@ public class LocalizerTest {
     @MockBean
     private ApplicationSettings appSettings;
 
-    @Autowired
+    @MockBean
     private MessageSource messageSource;
 
     @Autowired
@@ -38,6 +45,8 @@ public class LocalizerTest {
     void localizeListStringTest() {
         given(appSettings.getLocale())
                 .willReturn(ruLoc);
+        given(messageSource.getMessage(any(), any(), eq(ruLoc)))
+                .willReturn("Тестовая строка").willReturn("Другая тестовая строка");
 
         List<String> locList = localizer.localize(Arrays.asList("tst-string","tst-another-string"));
         assertEquals(2, locList.size());
@@ -46,6 +55,8 @@ public class LocalizerTest {
 
         given(appSettings.getLocale())
                 .willReturn(enLoc);
+        given(messageSource.getMessage(any(), any(), eq(enLoc)))
+                .willReturn("Test string").willReturn("Another test string");
         locList = localizer.localize(Arrays.asList("tst-string","tst-another-string"));
         assertEquals(2, locList.size());
         assertEquals("Test string", locList.get(0));
@@ -57,12 +68,16 @@ public class LocalizerTest {
     void localizeStringTest() {
         given(appSettings.getLocale())
                 .willReturn(enLoc);
+        given(messageSource.getMessage(any(), any(), eq(enLoc)))
+                .willReturn("Test string");
 
         String str = localizer.localize("tst-string");
         assertEquals("Test string", str);
 
         given(appSettings.getLocale())
                 .willReturn(ruLoc);
+        given(messageSource.getMessage(any(), any(), eq(ruLoc)))
+                .willReturn("Тестовая строка");
         str = localizer.localize("tst-string");
         assertEquals("Тестовая строка", str);
     }
@@ -72,11 +87,16 @@ public class LocalizerTest {
     void localizeStringWithParamTest() {
         given(appSettings.getLocale())
                 .willReturn(enLoc);
+        given(messageSource.getMessage(any(), eq(new Integer[]{5}), eq(enLoc)))
+                .willReturn("String with param 5");
+
         String str = localizer.localize("tst-string-param", 5);
         assertEquals("String with param 5", str);
 
         given(appSettings.getLocale())
                 .willReturn(ruLoc);
+        given(messageSource.getMessage(any(), eq(new Integer[]{5}), eq(ruLoc)))
+                .willReturn("Строка с параметром 5");
         str = localizer.localize("tst-string-param", 5);
         assertEquals("Строка с параметром 5", str);
     }
